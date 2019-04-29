94f2666b342f098026d6fcf5dfaa36f52ddba78d
Nick Weber <webernick@icloud.com>
2019-04-29 12:08:02 -0700

    Adds helper functions

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,15 @@
 // jshint esversion: 6
 
-console.log('Yup, we are working');
+// -------------------------------
+// GLOBAL CONSTANTS
+// -------------------------------
+const gallery = document.getElementById('gallery');
+let employeeData = [];
 
+
+// -------------------------------
+// FETCH FUNCTIONS
+// -------------------------------
 
 function fetchData(url) {
   return fetch(url)
@@ -11,15 +19,71 @@ function fetchData(url) {
     // .then(data => console.log(data.results[0]));
 }
 
-fetchData('https://randomuser.me/api/')
-  .then(function(data) {
-    var person = data.results[0];
-    console.log(`The person is fucking ${person.gender}`);
-  });
+
+fetchData('https://randomuser.me/api/?results=12')
+  .then(data => data.results)
+  .then(data => employeeData = data)
+  .then(data => generateGalleryItems(data));
 
 
-  // => data.results[0])
+// -------------------------------
+// HELPER FUNCTIONS
+// -------------------------------
+
+// generates and adds the gallery HTML from the request, and then adds the event listener
+function generateGalleryItems(data) {
+  let galleryHTML = '';
+  for (let item of data) {
+    let itemHTML = `
+      <div class="card ${item.login.uuid}">
+        <div class="card-img-container">
+            <img class="card-img" src="${item.picture.large}" alt="profile picture">
+        </div>
+        <div class="card-info-container">
+            <h3 id="name" class="card-name cap">${item.name.first} ${item.name.last}</h3>
+            <p class="card-text">${item.email}</p>
+            <p class="card-text cap">${item.location.city}, ${item.location.state}</p>
+        </div>
+      </div>`;
+      galleryHTML += itemHTML;
+  }
+  gallery.innerHTML = galleryHTML;
   
+  const cards = document.getElementsByClassName('card');
+  setListener(cards);
+}
 
 
-  // .then(person => console.log(`the person is ${person.gender}`));
+// gets card information, matches to the json data, builds and display html
+function displayItem(card) {
+  // get the uuid from the div class list
+  itemUUID = this.classList[1];
+  console.log(itemUUID);
+
+  // match data with card clicked
+  let itemData = {};
+    // let's use reduce method here instead of a loop
+
+  // build and insert HTML with data
+
+
+  // set the close button
+
+}
+
+
+
+
+
+// -------------------------------
+// LISTENER FUNCTIONS
+// -------------------------------
+
+// sets a click listener on each card div, runs after fetch promise resolved
+function setListener(cards) {
+  for (let card of cards) {
+    card.addEventListener('click', displayItem);
+  }
+}
+
+
